bba0637d094fa35048427cc0ef83dcd0553abb2c
Miracle Pepple <miracle4424@gmail.com>
2025-07-23 23:43:05 +0100

    Add .gitignore and fix index.js config

--- index.js
+++ index.js
@@ -1,130 +1,74 @@
-<<<<<<< HEAD
-// setting up the server
-const express = require("express");
-const app = express();
+// Load env vars
+require("dotenv").config();
 
+// Import dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Create express app
+const app = express();
 app.use(express.json());
 
-
-// setting up database and asking the app to listen
-
-const mongoose = require("mongoose");
-mongoose.connect (MONGODB_URL)
-.then (()=>{
-    console.log("MongoDB is connected...");
-    app.listen(8000, ()=>{
-    console.log(`Server is running on port ${PORT}`)
-});
-});
-
-// import student model
+// Load your Student model
 const Student = require("./studentModel");
 
-// GET request for all the students
-app.get("/all-students", async(req, res)=>{
-    const allStudents = await Student.find();
-    res.status(200).json({
-        message: "Success!",
-        allStudents
-    });
-});
+// Connect to MongoDB
+const MONGODB_URL = process.env.MONGODB_URL;
+const PORT = process.env.PORT || 8000;
 
-// POST request to add a student
-app.post("/add-student", async (req, res)=>{
-    const {firstName, lastName, studentClass, gender, height, age} = req.body;
-    const newStudent = new Student ({firstName, lastName, studentClass, gender, height, age});
-    await newStudent.save();
-    res.status(201).json({
-        message: "Success",
-        newStudent
-    })
-});
-
-// PUT request to edit student information
-app.put("/edit-student/:id", async (req, res)=>{
-    const {id} = req.params
-    const {studentClass, height, age} = req.body
-    const editedStudent = await Student.findByIdAndUpdate (
-        id,
-        {studentClass, height, age},
-        {new: true}
-    )
-    res.status(200).json({
-        message: "Success!",
-        editedStudent
-    });
-})
-
-// DELETE request to delete a student
-app.delete("/delete-student", async (req, res)=>{
-    const {id} = req.body
-    const deletedStudent = await Student.findByIdAndDelete (id);
-    res.status(200).json({
-        message: "Student deleted successfully!"
-    });
-=======
-// setting up the server
-const express = require("express");
-const app = express();
-
-app.use(express.json());
+console.log("MONGODB_URL:", process.env.MONGODB_URL);
+console.log("PORT:", process.env.PORT);
 
 
-// setting up database and asking the app to listen
-
-const mongoose = require("mongoose");
-mongoose.connect (MONGODB_URL)
-.then (()=>{
+mongoose.connect(MONGODB_URL)
+  .then(() => {
     console.log("MongoDB is connected...");
-    app.listen(8000, ()=>{
-    console.log(`Server is running on port ${PORT}`)
-});
-});
-
-// import student model
-const Student = require("./studentModel");
-
-// GET request for all the students
-app.get("/all-students", async(req, res)=>{
-    const allStudents = await Student.find();
-    res.status(200).json({
-        message: "Success!",
-        allStudents
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
+  })
+  .catch(err => console.error("MongoDB connection error:", err));
+
+// GET all students
+app.get("/all-students", async (req, res) => {
+  const allStudents = await Student.find();
+  res.status(200).json({
+    message: "Success!",
+    allStudents
+  });
 });
 
-// POST request to add a student
-app.post("/add-student", async (req, res)=>{
-    const {firstName, lastName, studentClass, gender, height, age} = req.body;
-    const newStudent = new Student ({firstName, lastName, studentClass, gender, height, age});
-    await newStudent.save();
-    res.status(201).json({
-        message: "Success",
-        newStudent
-    })
+// POST new student
+app.post("/add-student", async (req, res) => {
+  const { firstName, lastName, studentClass, gender, height, age } = req.body;
+  const newStudent = new Student({ firstName, lastName, studentClass, gender, height, age });
+  await newStudent.save();
+  res.status(201).json({
+    message: "Success",
+    newStudent
+  });
 });
 
-// PUT request to edit student information
-app.put("/edit-student/:id", async (req, res)=>{
-    const {id} = req.params
-    const {studentClass, height, age} = req.body
-    const editedStudent = await Student.findByIdAndUpdate (
-        id,
-        {studentClass, height, age},
-        {new: true}
-    )
-    res.status(200).json({
-        message: "Success!",
-        editedStudent
-    });
-})
+// PUT update student
+app.put("/edit-student/:id", async (req, res) => {
+  const { id } = req.params;
+  const { studentClass, height, age } = req.body;
+  const editedStudent = await Student.findByIdAndUpdate(
+    id,
+    { studentClass, height, age },
+    { new: true }
+  );
+  res.status(200).json({
+    message: "Success!",
+    editedStudent
+  });
+});
 
-// DELETE request to delete a student
-app.delete("/delete-student", async (req, res)=>{
-    const {id} = req.body
-    const deletedStudent = await Student.findByIdAndDelete (id);
-    res.status(200).json({
-        message: "Student deleted successfully!"
-    });
->>>>>>> f83aa602f8af1cd84eda110e8b05ebf6463263af
+// DELETE student (better to use param)
+app.delete("/delete-student/:id", async (req, res) => {
+  const { id } = req.params;
+  await Student.findByIdAndDelete(id);
+  res.status(200).json({
+    message: "Student deleted successfully!"
+  });
 });
